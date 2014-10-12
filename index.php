14c4f9603fe80dd98d687148a63730862c6b961a
Roy Segall <roy.segall@gizra.com>
2014-10-12 23:08:26 +0300

    Adding fucntions.

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
 
+function plus($first, $second) {
+	return $first + $second;
+}
+
+function minus($first, $second) {
+	return $first - $second;
+}
+
+function divide($firs, $second) {
+	return $first / $second;
+}
+
+function multiple($first, $second) {
+	return $first * $second;
+}
+
 echo "hello";
