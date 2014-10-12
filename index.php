e4e502d58ac1c767f4e940114bcabda1bce24e42
Roy Segall <roy.segall@gizra.com>
2014-10-12 23:14:12 +0300

    Conflict.

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
 
+class calc {
+
+  protected $first;
+  protected $second;
+
+  function plus() {
+    return $this->first + $this->second;
+  }
+
+  function minus() {
+    return $this->first - $this->second;
+  }
+
+  function divide() {
+    return $this->first / $this->second;
+  }
+
+  function multiple() {
+    return $this->first * $this->second;
+  }
+
+}
+
 echo "hello";
